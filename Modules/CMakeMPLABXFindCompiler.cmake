939576134e14ca379d2af41b6df0516120583bc3
Byran Wills-Heath <byran@adgico.co.uk>
2017-08-13 20:15:03 +0100

    Added search paths for programs when on Linux

--- Modules/CMakeMPLABXFindCompiler.cmake
+++ Modules/CMakeMPLABXFindCompiler.cmake
@@ -1,12 +1,16 @@
 
 find_program(CMAKE_C_COMPILER
   NAMES xc32-gcc
-  PATHS "C:/Program Files (x86)/Microchip/xc32/v${XC32_VERSION}/bin"
+  PATHS
+  "C:/Program Files (x86)/Microchip/xc32/v${XC32_VERSION}/bin"
+  "/opt/microchip/xc32/v${XC32_VERSION}/bin"
   NO_DEFAULT_PATH)
 mark_as_advanced(CMAKE_C_COMPILER)
 
 find_program(CMAKE_CXX_COMPILER
   NAMES xc32-g++
-  PATHS "C:/Program Files (x86)/Microchip/xc32/v${XC32_VERSION}/bin"
+  PATHS
+  "C:/Program Files (x86)/Microchip/xc32/v${XC32_VERSION}/bin"
+  "/opt/microchip/xc32/v${XC32_VERSION}/bin"
   NO_DEFAULT_PATH)
 mark_as_advanced(CMAKE_CXX_COMPILER)
--- Modules/CMakeMPLABXFindMake.cmake
+++ Modules/CMakeMPLABXFindMake.cmake
@@ -1,12 +1,20 @@
 
 find_program(CMAKE_MAKE_PROGRAM
-  NAMES make.exe
-  PATHS "C:/Program Files (x86)/Microchip/MPLABX/v3.26/gnuBins/GnuWin32/bin"
+  NAMES
+    make.exe
+    make
+  PATHS
+    "C:/Program Files (x86)/Microchip/MPLABX/v3.26/gnuBins/GnuWin32/bin"
+    "/usr/bin"
   NO_DEFAULT_PATH)
 
 mark_as_advanced(CMAKE_MAKE_PROGRAM)
 
 find_program(MPLABX_PROJECT_MAKEFILES_GENERATOR
-  NAMES prjMakefilesGenerator.bat
-  PATHS "C:/Program Files (x86)/Microchip/MPLABX/v3.26/mplab_ide/bin"
+  NAMES
+    prjMakefilesGenerator.bat
+    prjMakefilesGenerator.sh
+  PATHS
+    "C:/Program Files (x86)/Microchip/MPLABX/v3.26/mplab_ide/bin"
+    "/opt/microchip/mplabx/v3.65/mplab_ide/bin"
   NO_DEFAULT_PATH)
