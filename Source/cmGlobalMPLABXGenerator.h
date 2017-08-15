88114da1c42eabaf33ad53e4bd62188811e4bfa5
Byran Wills-Heath <byran@adgico.co.uk>
2017-08-15 20:32:42 +0100

    CMake global generator now indicates that it supports Toolsets for GUI

--- Source/cmGlobalMPLABXGenerator.h
+++ Source/cmGlobalMPLABXGenerator.h
@@ -24,7 +24,7 @@ public:
  * Utilized by the generator factory to determine if this generator
  * supports toolsets.
  */
-  static bool SupportsToolset() { return false; }
+  static bool SupportsToolset() { return true; }
 
   /**
    * Utilized by the generator factory to determine if this generator
