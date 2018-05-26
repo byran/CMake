3daa2b04b4b3ffe8d45f2a0c01d520e94803d2b0
Byran Wills-Heath <byran@adgico.co.uk>
2018-05-26 22:59:03 +0100

    Add finding ar and bin2hex for xc32

--- Modules/CMakeMPLABXFindCompiler.cmake
+++ Modules/CMakeMPLABXFindCompiler.cmake
@@ -14,3 +14,19 @@ find_program(CMAKE_CXX_COMPILER
   "/opt/microchip/xc32/v${XC32_VERSION}/bin"
   NO_DEFAULT_PATH)
 mark_as_advanced(CMAKE_CXX_COMPILER)
+
+find_program(CMAKE_AR
+        NAMES xc32-ar
+        PATHS
+        "C:/Program Files (x86)/Microchip/xc32/v${XC32_VERSION}/bin"
+        "/opt/microchip/xc32/v${XC32_VERSION}/bin"
+        NO_DEFAULT_PATH)
+mark_as_advanced(XC32_AR)
+
+find_program(XC32_BIN2HEX
+        NAMES xc32-bin2hex
+        PATHS
+        "C:/Program Files (x86)/Microchip/xc32/v${XC32_VERSION}/bin"
+        "/opt/microchip/xc32/v${XC32_VERSION}/bin"
+        NO_DEFAULT_PATH)
+mark_as_advanced(XC32_BIN2HEX)
